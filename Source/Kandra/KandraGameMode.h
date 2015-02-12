acfb259d0eefb8303ddb412cf50498d6da8a74d2
Jesper Sahlin <jesper.sahlin@gmail.com>
2015-02-12 14:04:33 +0100

    Start

---
+++ Source/Kandra/KandraGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "KandraGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KANDRA_API AKandraGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
